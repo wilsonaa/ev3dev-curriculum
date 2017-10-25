9a2281e74d9af61233d01a1cda4759b921a813c7
dreikobd <dreikobd@rose-hulman.edu>
2017-10-25 14:37:57 -0400

    fixed

--- sandbox/DREIKOBD/motors/m1_drive_timed.py
+++ sandbox/DREIKOBD/motors/m1_drive_timed.py
@@ -89,7 +89,7 @@ Authors: David Fisher and Benjamin Dreikosen.
 
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
-
+  
 # Done: 4. Change the input questions from:
 #   Enter a speed for the left motor (0 to 900 dps):
 #   Enter a speed for the right motor (0 to 900 dps):
