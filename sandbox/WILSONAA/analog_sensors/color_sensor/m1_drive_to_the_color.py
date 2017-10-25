051a6b9a78b2dd82551344149f9ed638ac34ac53
wilsonaa <wilsonaa@rose-hulman.edu>
2017-10-25 13:47:26 -0400

    adding assignment

--- sandbox/WILSONAA/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/WILSONAA/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -13,8 +13,8 @@ If the user presses the Down  button, the robot drives until the robot gets to B
 If the user presses the Left  button, the robot drives until the robot gets to Black.
 If the user presses the Right button, the robot drives until the robot gets to White.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Aaron Wilson.
+"""  # DonE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
