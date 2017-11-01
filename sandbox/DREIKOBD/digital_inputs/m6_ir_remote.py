46a4520e2481c9c22496831e72e459089d04dde1
wilsonaa <wilsonaa@rose-hulman.edu>
2017-10-31 21:42:27 -0400

    update

--- sandbox/DREIKOBD/digital_inputs/m6_ir_remote.py
+++ sandbox/DREIKOBD/digital_inputs/m6_ir_remote.py
@@ -25,8 +25,8 @@
     -- Pressing the Back button will allow your program to end.  It should stop motors, turn on both green LEDs, and
        then print and say Goodbye.  You will need to implement a new robot method called shutdown to handle this task.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Aaron Wilson and Ben Dreikoson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -61,7 +61,16 @@ def main():
     # TODO: 4. Add the necessary IR handler callbacks as per the instructions above.
     # Remote control channel 1 is for driving the crawler tracks around (none of these functions exist yet below).
     # Remote control channel 2 is for moving the arm up and down (all of these functions already exist below).
+    rc1 = ev3.RemoteControl(channel=1)
+    rc1.on_blue_up = lambda state: handle_blue_up_1(state, dc)
+    rc1.on_blue_down = lambda state: handle_blue_down_1(state, dc)
+    rc1.on_red_down = lambda state: handle_red_down_1(state, dc)
+    rc1.on_red_up = lambda state: handle_red_up_1(state, dc)
 
+    rc2 = ev3.RemoteControl(channel=2)
+    rc2.on_blue_up = lambda state: handle_blue_up(state, dc)
+    rc2.on_red_down = lambda state: handle_red_down(state, dc)
+    rc2.on_red_up = lambda state: handle_red_up(state, dc)
     # For our standard shutdown button.
     btn = ev3.Button()
     btn.on_backspace = lambda state: handle_shutdown(state, dc)
