5c5c1208177395cb204da4c47f4fa340d7c4d75a
wilsonaa <wilsonaa@rose-hulman.edu>
2017-10-31 21:02:57 -0400

    update

--- sandbox/DREIKOBD/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/DREIKOBD/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -73,18 +73,12 @@ def arm_calibration(arm_motor, touch_sensor):
     arm_motor.run_forever(speed_sp=900)
     while not touch_sensor.is_pressed:
         time.sleep(0.01)
-        break
     arm_motor.stop(stop_action = ev3.Motor.STOP_ACTION_BRAKE)
     ev3.Sound.beep().wait()
-    print('1')
     arm_revolutions_for_full_range = 14.2 * 360
-    print('2')
     arm_motor.run_to_abs_pos(position_sp=-arm_revolutions_for_full_range)
-    print('3')
-    arm_motor.wait_while(ev3.Motor.STATE_HOLDING)
-    print('4')
+    arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
     ev3.Sound.beep().wait()
-    print('5')
     arm_motor.position = 0  # Calibrate the down position as 0 (this line is correct as is).
 
 
