819effcb51543f8082acddffc2cb45565a4a0b7a
wilsonaa <wilsonaa@rose-hulman.edu>
2017-11-02 19:12:02 -0400

    update

--- sandbox/DREIKOBD/analog_sensors/ir_sensor/m2_beacon_seeking.py
+++ sandbox/DREIKOBD/analog_sensors/ir_sensor/m2_beacon_seeking.py
@@ -95,7 +95,17 @@ def seek_beacon(robot):
                 # You add more!
             if current_distance == 0:
                 return True
-            if current_distance< 0:
+            if current_heading < 0:
+                robot.drive_right_motor(400)
+                time.sleep(.2)
+                robot.stop()
+            if current_heading > 0:
+                robot.drive_left_motor(400)
+                time.sleep(.2)
+                robot.stop()
+
+
+
 
 
 
