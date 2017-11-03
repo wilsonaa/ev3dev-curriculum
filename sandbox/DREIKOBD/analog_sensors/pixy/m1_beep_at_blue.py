4c4d1724fe3eb6d073b68ec28fd9486d38e4695a
dreikobd <dreikobd@rose-hulman.edu>
2017-11-02 20:18:42 -0400

    Merger

--- sandbox/DREIKOBD/analog_sensors/pixy/m1_beep_at_blue.py
+++ sandbox/DREIKOBD/analog_sensors/pixy/m1_beep_at_blue.py
@@ -28,16 +28,20 @@ def main():
 
     robot = robo.Snatch3r()
     robot.pixy.mode = "SIG1"
-    
+
     while not robot.touch_sensor.is_pressed:
         # TODO: 2. Implement the module as described in the opening comment block.
         # It is recommended that you add to your Snatch3r class's constructor the pixy object, as shown
         #   self.pixy = ev3.Sensor(driver_name="pixy-lego")
         #   assert self.pixy
         # Then here you can use a command like width = robot.pixy.value(3)
-
-
-
+        print("value1:Y", robot.pixy.value(1))
+        print("value2:X", robot.pixy.value(2))
+        print("value3:Width", robot.pixy.value(3))
+        print("value4:Height", robot.pixy.value(4))
+        if robot.pixy.value(4) > 0:
+            ev3.Sound.beep()
+            time.sleep(1.0)
         time.sleep(0.1)
 
     print("Goodbye!")
