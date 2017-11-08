ebc51c25e51c8a3e8643d9c67f4d1ae07100ca0f
dreikobd <dreikobd@rose-hulman.edu>
2017-11-08 14:49:24 -0500

    Merger

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -221,3 +221,45 @@ class Snatch3r(object):
 
         print("Goodbye!")
         ev3.Sound.speak("Goodbye").wait()
+
+    def PlayingCatch(self):
+        print("--------------------------------------------")
+        print(" Playing Catch")
+        print("--------------------------------------------")
+        ev3.Sound.speak("Playing Catch").wait()
+        print("Press the touch sensor to exit this program.")
+
+        robot = robo.Snatch3r()
+        robot.pixy.mode = "SIG1"
+        turn_speed = 300
+
+        while not robot.touch_sensor.is_pressed:
+
+            sa = robot.pixy.value(3) * robot.pixy.value(4)
+            if robot.pixy.value(1) < 150:
+                robot.drive_left_motor(-turn_speed)
+                robot.drive_right_motor(turn_speed)
+            elif robot.pixy.value(1) > 170:
+                robot.drive_left_motor(turn_speed * .5)
+                robot.drive_right_motor(-turn_speed * .5)
+            else:
+                if sa > 2500:
+                    print("Caught Object")
+                    ev3.Sound.speak("Caught Object").wait()
+                    robot.stop()
+                    break
+                elif sa > 1800:
+                    robot.drive_forward(turn_speed * .25, turn_speed * .25)
+
+                elif sa > 800:
+                    robot.drive_forward(turn_speed * .5, turn_speed * .5)
+
+                elif sa > 500:
+                    robot.drive_forward(turn_speed * .75, turn_speed * .75)
+
+                elif sa < 100:
+                    robot.drive_forward(turn_speed, turn_speed)
+                time.sleep(0.25)
+
+        print("Goodbye!")
+        ev3.Sound.speak("Goodbye").wait()
--- projects/DREIKOBD/RobotCode.py
+++ projects/DREIKOBD/RobotCode.py
@@ -8,9 +8,9 @@ import robot_controller as robo
 
 def main():
     print("--------------------------------------------")
-    print(" Starting")
+    print(" Playing Catch")
     print("--------------------------------------------")
-    ev3.Sound.speak("Starting").wait()
+    ev3.Sound.speak("Playing Catch").wait()
     print("Press the touch sensor to exit this program.")
 
     robot = robo.Snatch3r()
@@ -19,13 +19,7 @@ def main():
 
     while not robot.touch_sensor.is_pressed:
 
-        # TODO: 2. Read the Pixy values for x and y
-        # Print the values for x and y
-        print("value1:X", robot.pixy.value(1))
-        print("value2:Y", robot.pixy.value(2))
-        print("value3:Width",robot.pixy.value(3))
-        print("value3:Hiegth", robot.pixy.value(4))
-        print("surface Area",(robot.pixy.value(3)*robot.pixy.value(4)))
+
         sa = robot.pixy.value(3)*robot.pixy.value(4)
         if robot.pixy.value(1) < 150:
             robot.drive_left_motor(-turn_speed)
