b5f047f38bdb60cf1c64b5aa84592eac340d3bcf
dreikobd <dreikobd@rose-hulman.edu>
2017-11-08 14:47:04 -0500

    Merger

--- projects/DREIKOBD/RobotCode.py
+++ projects/DREIKOBD/RobotCode.py
@@ -8,14 +8,14 @@ import robot_controller as robo
 
 def main():
     print("--------------------------------------------")
-    print(" Color tracking")
+    print(" Starting")
     print("--------------------------------------------")
-    ev3.Sound.speak("Color tracking").wait()
+    ev3.Sound.speak("Starting").wait()
     print("Press the touch sensor to exit this program.")
 
     robot = robo.Snatch3r()
     robot.pixy.mode = "SIG1"
-    turn_speed = 600
+    turn_speed = 300
 
     while not robot.touch_sensor.is_pressed:
 
@@ -27,23 +27,30 @@ def main():
         print("value3:Hiegth", robot.pixy.value(4))
         print("surface Area",(robot.pixy.value(3)*robot.pixy.value(4)))
         sa = robot.pixy.value(3)*robot.pixy.value(4)
-        if sa > 1000:
-            print("Caught Object")
-            ev3.Sound.speak("Caught Object").wait()
-            robot.stop()
-            break
-        elif sa > 850:
-            robot.drive_forward(turn_speed*.25,turn_speed*.25)
+        if robot.pixy.value(1) < 150:
+            robot.drive_left_motor(-turn_speed)
+            robot.drive_right_motor(turn_speed)
+        elif robot.pixy.value(1) > 170:
+            robot.drive_left_motor(turn_speed*.5)
+            robot.drive_right_motor(-turn_speed*.5)
+        else:
+            if sa > 2500:
+                print("Caught Object")
+                ev3.Sound.speak("Caught Object").wait()
+                robot.stop()
+                break
+            elif sa > 1800:
+                robot.drive_forward(turn_speed*.25,turn_speed*.25)
 
-        elif sa > 500:
-            robot.drive_forward(turn_speed*.5, turn_speed*.5)
+            elif sa > 800:
+                robot.drive_forward(turn_speed*.5, turn_speed*.5)
 
-        elif sa > 150:
-            robot.drive_forward(turn_speed*.75, turn_speed*.75)
+            elif sa > 500:
+                robot.drive_forward(turn_speed*.75, turn_speed*.75)
 
-        elif sa < 50:
-            robot.drive_forward(turn_speed, turn_speed)
-        time.sleep(0.25)
+            elif sa < 100:
+                robot.drive_forward(turn_speed, turn_speed)
+            time.sleep(0.25)
 
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
